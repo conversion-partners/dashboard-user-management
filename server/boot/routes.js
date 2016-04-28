25e5e65666e7c39c3c7c7b6e1ff942dcfa3b7f01
trimm <jesse@accountshifter.com>
2016-04-28 08:50:40 +0200

    cc

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -1,101 +1,47 @@
-var dsConfig = require('../datasources.json');
-
 module.exports = function(app) {
-  var User = app.models.user;
+  var router = app.loopback.Router();
 
-  //login page
-  app.get('/', function(req, res) {
-    var credentials = dsConfig.emailDs.transports[0].auth;
-    res.render('login', {
-      email: credentials.user,
-      password: credentials.pass
+  router.get('/', function(req, res) {
+    res.render('index', {
+      loginFailed: false
     });
   });
 
-  //verified
-  app.get('/verified', function(req, res) {
-    res.render('verified');
+  router.get('/projects', function(req, res) {
+    res.render('projects');
   });
 
-  //log a user in
-  app.post('/login', function(req, res) {
-    User.login({
-      email: req.body.email,
-      password: req.body.password
-    }, 'user', function(err, token) {
-      if (err) {
-        res.render('response', {
-          title: 'Login failed',
-          content: err,
-          redirectTo: '/',
-          redirectToLinkText: 'Try again'
-        });
-        return;
-      }
+  router.post('/projects', function(req, res) {
+    var email = req.body.email;
+    var password = req.body.password;
 
-      res.render('home', {
-        email: req.body.email,
+    app.models.User.login({
+      email: email,
+      password: password
+    }, 'user', function(err, token) {
+      if (err)
+        return res.render('index', {
+          email: email,
+          password: password,
+          loginFailed: true
+        });
+
+      token = token.toJSON();
+
+      res.render('projects', {
+        username: token.user.username,
         accessToken: token.id
       });
     });
   });
 
-  //log a user out
-  app.get('/logout', function(req, res, next) {
-    if (!req.accessToken) return res.sendStatus(401);
-    User.logout(req.accessToken.id, function(err) {
-      if (err) return next(err);
-      res.redirect('/');
-    });
+  router.get('/logout', function(req, res) {
+    var AccessToken = app.models.AccessToken;
+    var token = new AccessToken({id: req.query['access_token']});
+    token.destroy();
+
+    res.redirect('/');
   });
 
-  //send an email with instructions to reset an existing user's password
-  app.post('/request-password-reset', function(req, res, next) {
-    User.resetPassword({
-      email: req.body.email
-    }, function(err) {
-      if (err) return res.status(401).send(err);
-
-      res.render('response', {
-        title: 'Password reset requested',
-        content: 'Check your email for further instructions',
-        redirectTo: '/',
-        redirectToLinkText: 'Log in'
-      });
-    });
-  });
-
-  //show password reset form
-  app.get('/reset-password', function(req, res, next) {
-    if (!req.accessToken) return res.sendStatus(401);
-    res.render('password-reset', {
-      accessToken: req.accessToken.id
-    });
-  });
-
-  //reset the user's pasword
-  app.post('/reset-password', function(req, res, next) {
-    if (!req.accessToken) return res.sendStatus(401);
-
-    //verify passwords match
-    if (!req.body.password ||
-        !req.body.confirmation ||
-        req.body.password !== req.body.confirmation) {
-      return res.sendStatus(400, new Error('Passwords do not match'));
-    }
-
-    User.findById(req.accessToken.userId, function(err, user) {
-      if (err) return res.sendStatus(404);
-      user.updateAttribute('password', req.body.password, function(err, user) {
-      if (err) return res.sendStatus(404);
-        console.log('> password reset processed successfully');
-        res.render('response', {
-          title: 'Password reset success',
-          content: 'Your password has been reset successfully',
-          redirectTo: '/',
-          redirectToLinkText: 'Log in'
-        });
-      });
-    });
-  });
+  app.use(router);
 };
--- server/server.js
+++ server/server.js
@@ -1,18 +1,22 @@
-var loopback = require('loopback');
-var boot = require('loopback-boot');
-var path = require('path');
 var bodyParser = require('body-parser');
+var boot = require('loopback-boot');
+var loopback = require('loopback');
+var path = require('path');
 
 var app = module.exports = loopback();
 
-// configure view handler
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.middleware('initial', bodyParser.urlencoded({ extended: true }));
 
-// configure body parser
-app.use(bodyParser.urlencoded({extended: true}));
+// Bootstrap the application, configure models, datasources and middleware.
 
-app.use(loopback.token());
+boot(app, __dirname);
+
+app.set('view engine', 'ejs'); // LoopBack comes with EJS out-of-box
+app.set('json spaces', 2); // format json responses for easier viewing
+
+// must be set to serve views properly when starting the app via `slc run` from
+// the project root
+app.set('views', path.resolve(__dirname, 'views'));
 
 app.start = function() {
   // start the web server
@@ -27,12 +31,8 @@ app.start = function() {
   });
 };
 
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-  if (err) throw err;
+// start the server if `$ node server.js`
+if (require.main === module) {
+  app.start();
+}
 
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
-});
